c46f28e6267271d87914166b174b572422b1f84c
Mauricio <mauriciofagner2010@hotmail.com>
2020-08-21 21:01:27 -0300

    Adicionar as transações na tela

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ const transactionsDiv = document.querySelector("#transactions");
 
 const showTransactions = (transaction) => {
   let transactionType = transaction.amount > 0 ? 'plus' : 'minus';
-  console.log(transactionType);
+  console.log('A transação é:', transactionType);
+
+  let transactionElement = `
+    <li class=${transactionType}>
+      ${transaction.name}
+      <span>R$ ${transaction.amount}</span>
+      <button class="delete-btn">x</button>
+    </li>
+  `
+
+  transactionsDiv.innerHTML += transactionElement;
 };
 
 transactions.forEach(showTransactions);
