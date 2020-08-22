0446f990149bf621a6c33be694b94e70158fbcb0
Mauricio <mauriciofagner2010@hotmail.com>
2020-08-21 22:53:50 -0300

    Atualizar os valores de receitas e despesas

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let transactions = [
-  {id: 1, name: 'Bolo', amount: -200},
-  {id: 2, name: 'Aluguel', amount: 100}
-];
-
 const transactionsDiv = document.querySelector("#transactions");
+const balance = document.querySelector("#balance");
+const moneyPlus = document.querySelector("#money-plus");
+const moneyMinus = document.querySelector("#money-minus");
+
+let transactionsArray = [
+  {id: 1, name: 'Bolo', amount: -200},
+  {id: 2, name: 'Aluguel', amount: 100},
+  {id: 3, name: 'Teste', amount: 200}
+];
 
 const showTransactions = (transaction) => {
   let transactionType = transaction.amount > 0 ? 'plus' : 'minus';
-  console.log('A transação é:', transactionType);
 
   let transactionElement = `
     <li class=${transactionType}>
@@ -16,8 +19,32 @@ const showTransactions = (transaction) => {
       <button class="delete-btn">x</button>
     </li>
   `
-
   transactionsDiv.innerHTML += transactionElement;
+  updateBalance();
 };
 
-transactions.forEach(showTransactions);
+const updateBalance = () => {
+  totalEntries = transactionsArray.reduce((accumulator, { amount }) => {
+    if (amount > 0) {
+      accumulator += amount;
+    }
+
+    return accumulator;
+  }, 0);
+
+  totalExpenses = transactionsArray.reduce((accumulator, { amount }) => {
+    if (amount < 0) {
+      accumulator += amount;
+    }
+
+    return accumulator;
+  }, 0);
+
+  totalBalance = totalEntries + totalExpenses;
+
+  moneyPlus.innerHTML = `+${totalEntries}`;
+  moneyMinus.innerHTML = `${totalExpenses}`;
+  balance.innerHTML = `R$ ${totalBalance}`;
+}
+
+transactionsArray.forEach(showTransactions);
